e1c012527b845d35b986d47c4b0f71b08ad48eb9
Charlie Harrington <charrington68@gmail.com>
2023-06-19 08:48:00 -0700

    Adding more labels and icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,10 @@ export class AppComponent {
   @ViewChild('containerDiv' , {read: ElementRef, static: true}) container: ElementRef;
   @ViewChild('buttonDiv' , {read: ElementRef, static: true}) buttonDiv: ElementRef;
   title = 'wfui';
-  buttonText = ['Click Me!', 'Just Missed!', 'Try Again!', 'Almost Got It!', 'SOOOO Close!', 'Gotta Be Faster!'];
+  buttonText = ['Click Me!', 'Just Missed!', 'Try Again!', 'Almost Got It!', 'SOOOO Close!', 'Gotta Be Faster!', 'Once More!', 'Sorry!'];
+  buttonIcon = ['home', 'star', 'favorite', 'key', 'info', 'code', 'bug_report'];
   buttonId = 0;
+  iconId = 4;
   topVal = 50;
   top = '50px';
   leftVal = 100;
@@ -25,13 +27,14 @@ export class AppComponent {
     console.log(buttonRect);
     const minVertMove = Math.floor(buttonRect.height) + 1;
     const minHorizMove = Math.floor(buttonRect.width) + 1;
-    const maxVertLoc = containerRect.height - minVertMove;
-    const maxHorzLoc = containerRect.width - minHorizMove;
+    const maxVertLoc = containerRect.height - minVertMove - 10;
+    const maxHorzLoc = containerRect.width - minHorizMove - 10;
     this.topVal = this.getNewCoord(this.topVal, minVertMove, maxVertLoc);
     this.top = this.topVal.toString() + 'px';
     this.leftVal = this.getNewCoord(this.leftVal, minHorizMove, maxHorzLoc);
     this.left = this.leftVal.toString() + 'px';
     this.buttonId = this.randomIntFromInterval(0, this.buttonText.length - 1);
+    this.iconId = this.randomIntFromInterval(0, this.buttonIcon.length - 1);
   }
 
   getNewCoord(current: number, width: number, max: number): number {
